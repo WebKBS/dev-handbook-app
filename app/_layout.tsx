d245f942f0359e390b688742e32b2086a9c3c2ae
kangbeomsu <kbs21c1987@gmail.com>
2025-12-24 15:58:14 +0900

    set: 폰트 설치 및 설정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,47 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { Platform, StatusBar } from "react-native";
+
+SplashScreen.preventAutoHideAsync().then().catch(console.error);
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded, error] = useFonts({
+    "Pretendard-ExtraBold": require("../assets/fonts/Pretendard-ExtraBold.otf"),
+    "Pretendard-Bold": require("../assets/fonts/Pretendard-Bold.otf"),
+    "Pretendard-SemiBold": require("../assets/fonts/Pretendard-SemiBold.otf"),
+    "Pretendard-Medium": require("../assets/fonts/Pretendard-Medium.otf"),
+    "Pretendard-Regular": require("../assets/fonts/Pretendard-Regular.otf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync().then().catch(console.error);
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
+  return (
+    <>
+      {Platform.OS === "ios" ? (
+        <StatusBar
+          backgroundColor={"transparent"}
+          showHideTransition={"fade"}
+          animated={true}
+          barStyle={"default"}
+          translucent={true}
+        />
+      ) : (
+        <StatusBar
+          backgroundColor={"black"}
+          showHideTransition={"fade"}
+          animated={true}
+          barStyle={"default"}
+        />
+      )}
+    </>
+  );
 }
