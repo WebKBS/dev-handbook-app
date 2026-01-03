185361dc2aedc38c488cd022def9a1336372bc65
kangbeomsu <kbs21c1987@gmail.com>
2026-01-03 16:22:17 +0900

    refactor: SearchEmptyResult 컴포넌트 분리

--- components/card/SearchListCard.tsx
+++ components/card/SearchListCard.tsx
@@ -1,8 +1,6 @@
 import SearchResultCard from "@/components/card/SearchResultCard";
+import SearchEmptyResult from "@/components/empty/SearchEmptyResult";
 import SearchResultHeader from "@/components/header/SearchResultHeader";
-import { AppText } from "@/components/text/AppText";
-import { useTheme } from "@/providers/ThemeProvider";
-import { Feather } from "@expo/vector-icons";
 import { StyleSheet, View } from "react-native";
 
 export type SearchResult = {
@@ -17,26 +15,6 @@ export type SearchResult = {
 interface SearchListCardProps {
   filteredDocs: SearchResult[];
 }
-
-export const SearchEmptyResult = () => {
-  const { theme } = useTheme();
-  return (
-    <View
-      style={[
-        styles.emptyResult,
-        {
-          borderColor: theme.colors.border,
-          backgroundColor: theme.colors.cardBg,
-        },
-      ]}
-    >
-      <Feather name="search" size={16} color={theme.colors.muted} />
-      <AppText style={{ color: theme.colors.muted }}>
-        결과가 없습니다. 다른 키워드를 시도해 보세요.
-      </AppText>
-    </View>
-  );
-};
 
 const SearchListCard = ({ filteredDocs }: SearchListCardProps) => {
   return (
@@ -64,14 +42,5 @@ export default SearchListCard;
 const styles = StyleSheet.create({
   resultList: {
     gap: 12,
-  },
-
-  emptyResult: {
-    borderWidth: 1,
-    borderRadius: 14,
-    padding: 14,
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
   },
 });
